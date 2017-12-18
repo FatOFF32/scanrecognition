43181edce0d33a9b05dff12ed56c9cdbb512f0d4
FatOFF32 <fat-ne@yandex.ru>
2017-12-18 22:00:36 +0300

    one

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,37 @@
+import net.sourceforge.tess4j.ITessAPI.TessBaseAPI;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.Tesseract1;
+import net.sourceforge.tess4j.TesseractException;
+import net.sourceforge.tess4j.util.LoadLibs;
+
+import java.io.File;
+
 public class Main {
     public static void main(String[] args) {
+
+        File imageFile = new File("D:\\Учеба JAVA\\ДЗ\\img1.jpg");
+        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+//        ITesseract instance = new Tesseract1(); // JNA Direct Mapping
+//        File tessDataFolder = LoadLibs.extractTessResources("tessdata"); // Maven build only; only English data bundled
+//        instance.setDatapath(tessDataFolder.getAbsolutePath());
+        instance.setLanguage("rus");
+        try {
+            String result = instance.doOCR(imageFile);
+            System.out.println(result);
+        } catch (TesseractException e) {
+            System.err.println(e.getMessage());
+        }
+
+/*
+        TessBaseAPI tessBaseApi = new TessBaseAPI();
+        tessBaseApi.init(DATA_PATH, "eng");
+        tessBaseApi.setImage(bitmap);
+        String extractedText = tessBaseApi.getUTF8Text();
+        tessBaseApi.end();
+        return extractedText;
+*/
+
 
     }
 }
