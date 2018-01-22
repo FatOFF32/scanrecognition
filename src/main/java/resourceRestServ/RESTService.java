e3401b9b285fe559e9533cbe1658812c8b6e5144
fatov_di <fat-ne@yandex.ru>
2018-01-22 17:37:33 +0300

    one

--- src/main/java/resourceRestServ/RESTService.java
+++ src/main/java/resourceRestServ/RESTService.java
@@ -1,13 +1,20 @@
 package resourceRestServ;
 
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.node.ObjectNode;
 import root.ProcessMonitor;
+import root.WantedValues;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 
 @Path("recognizer")
 public class RESTService {
@@ -40,6 +47,77 @@ public class RESTService {
 
         return Response.status(Response.Status.OK)
                 .entity(response)
+                .build();
+    }
+
+
+    @POST
+    @Path("recognizeFile")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response recognizeFile(String requestJson) {
+
+        String responseJson = "";
+
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode rootNode = null;
+        try {
+            rootNode = mapper.readValue(requestJson, JsonNode.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ObjectNode responseObj = mapper.createObjectNode();
+
+        if (rootNode == null || rootNode.get("filePath") == null) {
+
+            responseObj.put("Error", true);
+            responseObj.put("ErrorText", "Не задан путь распознователя");
+            responseObj.put("RecognizedText", "");
+
+            try {
+                responseJson = mapper.writeValueAsString(responseObj);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(responseJson)
+                    .build();
+        }
+
+        String resultRecognize;
+
+        ITesseract instance = new Tesseract();  // JNA Interface Mapping
+        instance.setLanguage("rus");
+        try {
+            resultRecognize = instance.doOCR(new File(rootNode.get("filePath").asText()));
+        } catch (TesseractException e) {
+//            System.err.println(e.getMessage());
+            responseObj.put("Error", true);
+            responseObj.put("ErrorText", e.getMessage());
+            responseObj.put("RecognizedText", "");
+
+            try {
+                responseJson = mapper.writeValueAsString(responseObj);
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(responseJson)
+                    .build();
+        }
+
+        responseObj.put("Error", false);
+        responseObj.put("ErrorText", "");
+        responseObj.put("RecognizedText", resultRecognize);
+
+        try {
+            responseJson = mapper.writeValueAsString(responseObj);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Response.status(Response.Status.OK)
+                .entity(responseJson)
                 .build();
     }
 
